SeeSpotRun/rmlint

commit bd757241e965ab133adb3508adb8412d9fa99340

preprocess: kick unique size files out a bit earlier

lib/session.c
@@ -180,13 +180,15 @@ static void rm_session_traverse_pipe(RmFile *file, RmSession *session) {
  * Assumes safe access to session->tables->other_lint and session->counters.
  */
 static void rm_session_pp_files_pipe(RmFile *file, RmSession *session) {
-    rm_assert_gentle(file->lint_type <= RM_LINT_TYPE_DUPE_CANDIDATE);
-
     if(file->lint_type == RM_LINT_TYPE_DUPE_CANDIDATE) {
         /* bundled hardlink is counted as filtered file */
         rm_assert_gentle(file->hardlinks.hardlink_head);
         session->counters->total_filtered_files--;
+    } else if(file->lint_type == RM_LINT_TYPE_UNIQUE_FILE) {
+        session->counters->total_filtered_files--;
+        rm_fmt_write(file, session->formats, 1);
     } else if(file->lint_type >= RM_LINT_TYPE_OTHER) {
+        rm_assert_gentle(file->lint_type <= RM_LINT_TYPE_DUPE_CANDIDATE);
         /* filtered reject based on mtime, --keep, etc */
         session->counters->total_filtered_files--;
         rm_file_destroy(file);

lib/preprocess.c
@@ -242,6 +242,15 @@ static GSList *rm_preprocess_size_group(GSList *head, RmPPSession *preprocessor)
             head = next;
         }
     }
+    if(result && !result->next && !((RmFile *)result->data)->hardlinks.is_head) {
+        /* singleton group; discard */
+        RmFile *file = result->data;
+        file->lint_type = RM_LINT_TYPE_UNIQUE_FILE;
+        g_thread_pool_push(preprocessor->preprocess_file_pipe, file, NULL);
+        g_slist_free(result);
+        result = NULL;
+    }
+
     return result;
 }
 

commit a10d21a32dbeb78b8d541c4fae500a37f4e284cc

session: fix memory leak introduced by bd75724

lib/session.c
@@ -187,6 +187,7 @@ static void rm_session_pp_files_pipe(RmFile *file, RmSession *session) {
     } else if(file->lint_type == RM_LINT_TYPE_UNIQUE_FILE) {
         session->counters->total_filtered_files--;
         rm_fmt_write(file, session->formats, 1);
+        rm_file_destroy(file);
     } else if(file->lint_type >= RM_LINT_TYPE_OTHER) {
         rm_assert_gentle(file->lint_type <= RM_LINT_TYPE_DUPE_CANDIDATE);
         /* filtered reject based on mtime, --keep, etc */
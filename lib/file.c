SeeSpotRun/rmlint

commit a50ab2f46ffe813b24985939b6f96ba9cf6bdd4f

traverse: slightly reduce stat() calls

lib/preprocess.c
@@ -133,6 +133,28 @@ static guint rm_path_double_hash(const RmPathDoubleKey *key) {
     return rm_node_hash(key->file);
 }
 
+static bool rm_path_have_same_parent(
+    RmCfg *cfg, RmPathDoubleKey *key_a, RmPathDoubleKey *key_b) {
+    RmFile *file_a = key_a->file, *file_b = key_b->file;
+
+    if(cfg->use_meta_cache) {
+        if(key_a->parent_inode_set && key_b->parent_inode_set) {
+            RM_DEFINE_PATH(file_a);
+            RM_DEFINE_PATH(file_b);
+
+            key_a->parent_inode = rm_util_parent_node(file_a_path);
+            key_a->parent_inode_set = TRUE;
+
+            key_b->parent_inode = rm_util_parent_node(file_b_path);
+            key_b->parent_inode_set = TRUE;
+        }
+
+        return key_a->parent_inode == key_b->parent_inode;
+    } else {
+        return file_a->folder->parent == file_b->folder->parent;
+    }
+}
+
 static gboolean rm_path_double_equal(RmPathDoubleKey *key_a, RmPathDoubleKey *key_b) {
     if(key_a->file->inode != key_b->file->inode) {
         return FALSE;
@@ -145,21 +167,7 @@ static gboolean rm_path_double_equal(RmPathDoubleKey *key_a, RmPathDoubleKey *ke
     RmFile *file_a = key_a->file;
     RmFile *file_b = key_b->file;
 
-    if(key_a->parent_inode_set == false) {
-        RM_DEFINE_PATH(file_a);
-
-        key_a->parent_inode = rm_util_parent_node(file_a_path);
-        key_a->parent_inode_set = TRUE;
-    }
-
-    if(key_b->parent_inode_set == false) {
-        RM_DEFINE_PATH(file_b);
-
-        key_b->parent_inode = rm_util_parent_node(file_b_path);
-        key_b->parent_inode_set = TRUE;
-    }
-
-    if(key_a->parent_inode != key_b->parent_inode) {
+    if(!rm_path_have_same_parent(file_a->session->cfg, key_a, key_b)) {
         return FALSE;
     }
 

lib/traverse.c
@@ -399,8 +399,7 @@ static void rm_traverse_directory(RmTravBuffer *buffer, RmTravSession *trav_sess
                                      p->fts_path);
                     }
 
-                    RmStat dummy_buf;
-                    if(rm_sys_stat(p->fts_path, &dummy_buf) == -1 && errno == ENOENT) {
+                    if(access(p->fts_path, R_OK) == -1 && errno == ENOENT) {
                         /* Oops, that's a badlink. */
                         if(cfg->find_badlinks) {
                             ADD_FILE(RM_LINT_TYPE_BADLINK, false);

commit 1198387e51176e456059cbcfd40b62515d5929ad

preprocess: rework path double detection (broken since a50ab2f)

lib/file.c
@@ -338,3 +338,49 @@ gint rm_file_cmp_reverse_alphabetical(const RmFile *a, const RmFile *b) {
     RM_DEFINE_PATH(b);
     return g_strcmp0(b_path, a_path);
 }
+
+gint rm_file_cmp_pathdouble(RmFile *a, RmFile *b) {
+    rm_assert_gentle(a->dev == b->dev);
+    rm_assert_gentle(a->inode == b->inode);
+
+    /* a couple of cheap tests before we resort to parent inode checks */
+    if(a->folder == b->folder) {
+        /* identical paths (or pathtricia is broken) */
+        return 0;
+    }
+    gint result = g_strcmp0(a->folder->basename, b->folder->basename);
+    if(result != 0) {
+        /* files with differing basenames are not path doubles */
+        return result;
+    }
+    RmNode *pa = a->folder->parent;
+    RmNode *pb = b->folder->parent;
+    if(pa == pb) {
+        /* files have same basename and identical parent folders */
+        return 0;
+    }
+
+    /* final test is to compare parent folder dev & inodes; this will detect
+     * for example the same file in two different mountpoints.  To get parent
+     * inodes we first build the parent paths and then stat them.
+     * Some speedup options include storing this data in the pathtricia tree
+     * during traversal
+     */
+    char parent_path[PATH_MAX];
+    rm_trie_build_path((RmTrie *)&a->cfg->file_trie, pa, parent_path, PATH_MAX);
+    RmStat stat_buf_a;
+    int retval = rm_sys_stat(parent_path, &stat_buf_a);
+    rm_assert_gentle(retval != -1);
+
+    rm_trie_build_path((RmTrie *)&b->cfg->file_trie, pb, parent_path, PATH_MAX);
+    RmStat stat_buf_b;
+    retval = rm_sys_stat(parent_path, &stat_buf_b);
+    rm_assert_gentle(retval != -1);
+
+    result = SIGN_DIFF(stat_buf_a.st_dev, stat_buf_b.st_dev);
+    if(result != 0) {
+        /* should probably never happen since a and be are on same dev */
+        return result;
+    }
+    return SIGN_DIFF(stat_buf_a.st_ino, stat_buf_b.st_ino);
+}

lib/file.h
@@ -353,4 +353,10 @@ gint rm_file_node_cmp(const RmFile *file_a, const RmFile *file_b);
  */
 gint rm_file_cmp_reverse_alphabetical(const RmFile *a, const RmFile *b);
 
+/**
+ * @brief Rank two files with identical inode in an order which
+ * distinguishes path doubles
+ */
+gint rm_file_cmp_pathdouble(RmFile *a, RmFile *b);
+
 #endif /* end of include guard */

lib/preprocess.c
@@ -39,67 +39,8 @@ typedef struct RmPPSession {
     GThreadPool *preprocess_file_pipe;
 } RmPPSession;
 
-/* GHashTable key tuned to recognize duplicate paths.
- * i.e. RmFiles that are not only hardlinks but
- * also point to the real path
- */
-typedef struct RmPathDoubleKey {
-    /* stat(dirname(file->path)).st_ino */
-    ino_t parent_inode;
-
-    /* File the key points to */
-    RmFile *file;
-
-} RmPathDoubleKey;
-
-static gpointer rm_path_double_hash(const RmPathDoubleKey *key) {
-    /* depend only on the always set components, never change the hash duringthe run */
-    return (gpointer)key->file->folder->parent;
-}
-
-static bool rm_path_have_same_parent(RmPathDoubleKey *key_a, RmPathDoubleKey *key_b) {
-    RmFile *file_a = key_a->file, *file_b = key_b->file;
-    return file_a->folder->parent == file_b->folder->parent;
-}
-
-static gboolean rm_path_double_equal(RmPathDoubleKey *key_a, RmPathDoubleKey *key_b) {
-    if(key_a->file->inode != key_b->file->inode) {
-        return FALSE;
-    }
-
-    if(key_a->file->dev != key_b->file->dev) {
-        return FALSE;
-    }
-
-    RmFile *file_a = key_a->file;
-    RmFile *file_b = key_b->file;
-
-    if(!rm_path_have_same_parent(key_a, key_b)) {
-        return FALSE;
-    }
-
-    return g_strcmp0(file_a->folder->basename, file_b->folder->basename) == 0;
-}
-
-static RmPathDoubleKey *rm_path_double_new(RmFile *file) {
-    RmPathDoubleKey *key = g_malloc0(sizeof(RmPathDoubleKey));
-    key->file = file;
-    return key;
-}
-
-static void rm_path_double_free(RmPathDoubleKey *key) {
-    g_free(key);
-}
-
 RmFileTables *rm_file_tables_new(void) {
     RmFileTables *tables = g_slice_new0(RmFileTables);
-
-    tables->unique_paths_table =
-        g_hash_table_new_full((GHashFunc)rm_path_double_hash,
-                              (GEqualFunc)rm_path_double_equal,
-                              (GDestroyNotify)rm_path_double_free,
-                              NULL);
-
     g_mutex_init(&tables->lock);
     return tables;
 }
@@ -136,24 +77,20 @@ static bool rm_pp_handle_other_lint(RmFile *file, const RmPPSession *preprocesso
     return TRUE;
 }
 
-static bool rm_pp_check_path_double(RmFile *file, RmPPSession *preprocessor) {
-    RmPathDoubleKey *key = rm_path_double_new(file);
+/* this is slightly annoying but enables use of rm_util_slist_foreach_remove */
+typedef struct {
+    RmFile *prev_file;
+    GThreadPool *preprocess_file_pipe;
+} RmPPPathDoubleBuffer;
 
-    /* Lookup if there is a file with the same path */
-    RmPathDoubleKey *match_double_key =
-        g_hash_table_lookup(preprocessor->tables->unique_paths_table, key);
-
-    if(match_double_key == NULL) {
-        g_hash_table_add(preprocessor->tables->unique_paths_table, key);
-        return FALSE;
+static bool rm_pp_check_path_double(RmFile *file, RmPPPathDoubleBuffer *buf) {
+    if(buf->prev_file && rm_file_cmp_pathdouble(file, buf->prev_file) == 0) {
+        file->lint_type = RM_LINT_TYPE_PATHDOUBLE;
+        g_thread_pool_push(buf->preprocess_file_pipe, file, NULL);
+        return TRUE;
     }
-    RmFile *match_double = match_double_key->file;
-    rm_assert_gentle(match_double != file);
-
-    rm_path_double_free(key);
-    file->lint_type = RM_LINT_TYPE_PATHDOUBLE;
-    g_thread_pool_push(preprocessor->preprocess_file_pipe, file, NULL);
-    return TRUE;
+    buf->prev_file = file;
+    return FALSE;
 }
 
 #define RM_SLIST_LEN_GT_1(list) (list) && (list)->next
@@ -172,10 +109,12 @@ static RmFile *rm_preprocess_cluster(GSList *cluster, RmPPSession *preprocessor)
         /* there is a cluster of inode matches */
 
         /* remove path doubles */
-        rm_util_slist_foreach_remove(&cluster, (RmRFunc)rm_pp_check_path_double,
-                                     preprocessor);
-        /* clear the hashtable ready for the next cluster */
-        g_hash_table_remove_all(preprocessor->tables->unique_paths_table);
+        cluster = g_slist_sort(cluster, (GCompareFunc)rm_file_cmp_pathdouble);
+
+        RmPPPathDoubleBuffer buf;
+        buf.prev_file = NULL;
+        buf.preprocess_file_pipe = preprocessor->preprocess_file_pipe;
+        rm_util_slist_foreach_remove(&cluster, (RmRFunc)rm_pp_check_path_double, &buf);
     }
 
     /* process and remove other lint */
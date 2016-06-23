d6f681eace884121ee9c0c94873fa1c37b67d081
SeeSpotRun <thomas_d_j@yahoo.com.au>
2016-06-24 08:36:11 +1000

    tests: split path_max tests into separate procedures

--- tests/test_robustness/test_path_max.py
+++ tests/test_robustness/test_path_max.py
@@ -5,35 +5,48 @@ from tests.utils import *
 
 
 @with_setup(usual_setup_func, usual_teardown_func)
-def test_path_max():
-    create_file('xxx', 'folder/ok_file')
-    create_file('xxx', 'folder/too_long')
-    create_dirs(       'empty/ok_dir_')   # dirs need one more char for trailing '/'
-    create_dirs(       'empt2/too_long')
-
-    path_max = len(TESTDIR_NAME + '/folder/ok_file') + 1  # (+1 is for terminating null)
-
-
-    # first test won't be able to stat the 'too_long' files
-    head, *data, footer = run_rmlint('--pathmax', str(path_max))
-    assert 2 == len(data)
-    assert 2 == sum(find['type'] == 'emptydir' for find in data)  # empty and empty/ok_dir_
-
-
-    # first test adds 1 character to path_max and should run ok
-    head, *data, footer = run_rmlint('--pathmax', str(path_max + 1))
-    assert 6 == len(data)
-    assert 4 == sum(find['type'] == 'emptydir' for find in data)
-    assert 2 == sum(find['type'] == 'duplicate_file' for find in data)
-
-
-    # test for vulnerabilities / OBOB for passed paths close to path_max
-    path_max = len(TESTDIR_NAME) + 1  # (+1 is for terminating null)
-    head, *data, footer = run_rmlint('--pathmax', str(path_max))
-    assert 0 == len(data)
+def test_file():
+    name1 = 'is_seen'
+    name2 = 'too_long'
+    create_file('xxx', name1)
+    create_file('xxx', name2)
+    path_max = len(TESTDIR_NAME + '/' + name2) + 1  # (+1 is for terminating null)
 
     head, *data, footer = run_rmlint('--pathmax', str(path_max - 1))
     assert 0 == len(data)
 
+    head, *data, footer = run_rmlint('--pathmax', str(path_max))
+    assert 2 == len(data)
+
+
+@with_setup(usual_setup_func, usual_teardown_func)
+def test_dir():
+    name1 = 'empty1/is_seen'
+    name2 = 'empty2/too_long'
+    create_dirs(name1)
+    create_dirs(name2)
+
+    # +1 is for terminating null;
+    # +2 is so we can read the '..' dirent as part of checking that dir is empty
+    path_max = len(TESTDIR_NAME + '/' + name2 + '/') + 1 + 2 #
+
+    head, *data, footer = run_rmlint('--pathmax', str(path_max - 1))
+    assert 2 == len(data) # empty1 and empty1/is_seen
+
+    head, *data, footer = run_rmlint('--pathmax', str(path_max))
+    assert 5 == len(data) # all 4 created dirs plus the testdir itself
+
+@with_setup(usual_setup_func, usual_teardown_func)
+def test_passed():
+
+    # test for vulnerabilities / OBOB for passed paths close to path_max
+    # not looking for any results, just a chance to detect valgrind errors
+    create_file('xxx', 'a')
+
+    path_max = len(TESTDIR_NAME + '/') + 1  # (+1 for null)
+    head, *data, footer = run_rmlint('--pathmax', str(path_max))
+    assert 0 == len(data)
+    head, *data, footer = run_rmlint('--pathmax', str(path_max - 1))
+    assert 0 == len(data)
     head, *data, footer = run_rmlint('--pathmax', str(path_max + 1))
     assert 0 == len(data)
